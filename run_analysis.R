3bd8e3f0b76c75b44f1969af87d92d6a5e7c84ea
iansafiee <iansafiee@gmail.com>
2017-01-07 15:05:41 -0600

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -84,8 +84,8 @@ colnames(total) = variable_names
 setwd(original_path)
 total2 = aggregate(total[,3:68], by= list(total[,1], total[,2]), mean)
 colnames(total2) = names(total)
-write.table(total2, file = "./tidy_data", row.name = FALSE)
+write.table(total2, file = "./tidy_data", row.names = FALSE)
 variable_names = colnames(total2)
-write.table(variable_names, files = "./data")
+write.table(variable_names, file = "./data")
 
 
